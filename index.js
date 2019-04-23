b792b924fba308a73507e511f22ecce3d375f347
Nikki A <nikki.apostolakis@gmail.com>
2019-04-23 16:13:46 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,75 @@
+let store = {drivers:[], passengers:[], trips:[]};
+
+let driverId = 0;
+class Driver
+{
+  constructor (name)
+  {
+    this.id = ++driverId;
+    this.name = name;
+    store.drivers.push(this)
+  }
+
+  trips()
+  {
+    return store.trips.filter(function(trip){return trip.driverId == this.id}.bind(this));
+  }
+  passengers()
+  {
+    let passengers = [];
+    for (let trip of this.trips())
+    {
+        passengers.push(trip.passenger());
+    }
+    return passengers;
+
+  }
+}
+
+let passengerId = 0;
+class Passenger
+{
+  constructor (name)
+  {
+    this.id = ++passengerId;
+    this.name = name;
+    store.passengers.push(this)
+  }
+  trips()
+  {
+    return store.trips.filter(function(trip){return trip.passengerId == this.id}.bind(this));
+  }
+
+  drivers()
+  {
+    let drivers = [];
+    for (let trip of this.trips())
+    {
+        drivers.push(trip.driver());
+    }
+    return drivers;
+
+  }
+
+}
+
+let tripId = 0;
+class Trip
+{
+  constructor (driver, passenger)
+  {
+    this.id = ++tripId;
+    this.driverId = driver.id;
+    this.passengerId = passenger.id;
+    store.trips.push(this)
+  }
+
+  driver()
+  {
+    return store.drivers.find(function(driver){return driver.id == this.driverId}.bind(this));
+  }
+  passenger()
+  {
+    return store.passengers.find(function(passenger){return passenger.id == this.passengerId}.bind(this));
+  }
+}
